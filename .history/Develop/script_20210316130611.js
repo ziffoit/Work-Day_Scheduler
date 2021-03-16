b92c4b8b12a07267885e648710c58390de9fc148
Kevin Hanson <ziffoit@gmail.com>
2021-03-16 16:22:28 -0600

    moment

---
+++ .history/Develop/script_20210316130611.js
@@ -0,0 +1,6 @@
+var currentDay = $('#currentDay')
+
+function dateAndTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    currentDay.text(rightNow);
+  }
---
+++ Develop/script.js
@@ -0,0 +1,8 @@
+var currentDay = $('#currentDay')
+
+function dateAndTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    currentDay.text(rightNow);
+  }
+
+  setInterval(dateAndTime, 1000)
